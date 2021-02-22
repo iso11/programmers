0ba9b038e3c0b9ee408583e3e4b130145921363a
yunny <wiselsy60@gmail.com>
2021-02-22 23:28:23 +0900

    42584

--- Stack_Queue/lessons42584.py
+++ Stack_Queue/lessons42584.py
@@ -1,12 +1,14 @@
 def solution(prices):
-    answer = [0]*len(prices)
+    answer = []
 
-    for i in range(0, len(prices)):
-        for j in range(i+1, len(prices)):
-            answer[i] += 1
+    for i in range(0, len(prices)-1):
+        time = 1
+        for j in range(i+1, len(prices)-1):
             if (prices[i] > prices[j]): break
+            else: time += 1
+        answer.append(time)
 
+    answer.append(0)
     return answer
-
 
 print(solution([1, 2, 3, 2, 3]))
